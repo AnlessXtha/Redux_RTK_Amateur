1200029a23158c51f0c8f794fd798526c12a8c70
AnlessXtha <aadesh.shrrstha@gmail.com>
2023-11-24 23:00:34 +0545

    used axios, createAsyncThunk and extraReducers(builder)

--- 3/src/features/posts/PostsList.jsx
+++ 3/src/features/posts/PostsList.jsx
@@ -1,31 +1,46 @@
-import { useSelector } from "react-redux";
-import { selectAllPosts } from "./postsSlice";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectAllPosts,
+  getPostsStatus,
+  getPostsError,
+  fetchPosts,
+} from "./postsSlice";
+import { useEffect } from "react";
+
+import PostsExerpt from "./PostsExerpt";
 
 const PostsList = () => {
-    const posts = useSelector(selectAllPosts)
+  const dispatch = useDispatch();
 
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+  const posts = useSelector(selectAllPosts);
+  const postsStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0, 100)}</p>
-            <p className="postCredit">
-                <PostAuthor userId={post.userId} />
-                <TimeAgo timestamp={post.date} />
-            </p>
-            <ReactionButtons post={post} />
-        </article>
-    ))
+  useEffect(() => {
+    if (postsStatus === "idle") {
+      dispatch(fetchPosts());
+    }
+  }, [postsStatus, dispatch]);
 
-    return (
-        <section>
-            <h2>Posts</h2>
-            {renderedPosts}
-        </section>
-    )
-}
-export default PostsList
+  let content;
+  if (postsStatus === "loading") {
+    content = <p>"Loading"</p>;
+  } else if (postsStatus === "succeeded") {
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExerpt key={post.id} post={post} />
+    ));
+  } else if (postsStatus === "failed") {
+    content = <p>{error}</p>;
+  }
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {content}
+    </section>
+  );
+};
+export default PostsList;
--- 3/src/index.jsx
+++ 3/src/index.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import { fetchUsers } from "./features/users/usersSlice";
 
-ReactDOM.render(
+store.dispatch(fetchUsers());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
