c2a4f56d694ce2e82d4779c76867c235b0a2f19e
AnlessXtha <aadesh.shrrstha@gmail.com>
2023-12-05 17:42:08 +0545

    added the getPosts endpoints method

--- 7_Advanced_RTKquery/src/features/posts/postsSlice.jsx
+++ 7_Advanced_RTKquery/src/features/posts/postsSlice.jsx
@@ -8,7 +8,37 @@ const postsAdapter = createEntityAdapter({
 
 const initialState = postsAdapter.getInitialState();
 
-export const extendedApiSlice = postsAdapter.getInitialState();
+export const extendedApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts",
+      transformResponse: (responseData) => {
+        let min = 1; // to increment with 1 minute so that the all the post can have different times
+        const loadedPosts = responseData.map((post) => {
+          if (!post?.date)
+            post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          if (!post?.reactions)
+            post.reactions = {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            };
+          return post;
+        });
+        // .setAll normalizes our data and normalized data would have an array of just the ids and then it has an entites array as well and its really an entity's object that has a bunch of object in it, in each post and then we can use that ids array as an object lookup with the entities when we refer to each post.
+        return postsAdapter.setAll(initialState, loadedPosts);
+      },
+
+      // providesTags is used when any one of post ids is invalidated, it will refetch our list automatically.
+      providesTags: (result, error, arg) => [
+        { type: "Post", id: "LIST" },
+        ...result.ids.map((id) => ({ type: "Post", id })),
+      ],
+    }),
+  }),
+});
 
 //getSelectors creates these selectors and we rename them with aliases using destructuring
 export const {
